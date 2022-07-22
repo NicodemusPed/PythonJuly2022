98e8f4bffc87e8110d634b9140249e8c682837f3
NicodemusPed <107951768+NicodemusPed@users.noreply.github.com>
2022-07-21 19:38:40 -0700

    s

--- flask_mysql/Dojos_and_Ninjas/flask_app/controllers/ninjas.py
+++ flask_mysql/Dojos_and_Ninjas/flask_app/controllers/ninjas.py
@@ -1,16 +1,15 @@
-from flask import render_template, render_template, redirect
-
+from flask import render_template, redirect
 from flask_app import app
-from flask_app.models.ninja import ninjas
+from flask_app.models.ninja import ninja
 
 
 @app.route('/')
 def index():
-    return redirect('/dojos')
+    return redirect('/ninjas')
 
 @app.route('/ninjas')
 def ninjas():
-    return render_template("ninjas.html")
+    return render_template("ninjas.html",ninjas=Ninja.get_all())
 
 
 
--- flask_mysql/Dojos_and_Ninjas/flask_app/models/dojo.py
+++ flask_mysql/Dojos_and_Ninjas/flask_app/models/dojo.py
@@ -1,5 +1,6 @@
 from flask_app.config.mysqlconnection import connectToMySQL
-class Dojo:
+
+class Dojos:
     def __init__(self, data):
         self.id = data['id']
         self.name = data['name']
